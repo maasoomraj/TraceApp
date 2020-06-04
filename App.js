1d44da2cd0e07807e5950d44583166f9679aeee9
Jashpatel1 <patel.5@iitj.ac.in>
2020-06-04 15:49:31 +0530

    added fetch

--- App.js
+++ App.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, TextInput, Button,Alert, FlatList } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Trace a Product</Text>
-      <Text>Enter Product Name</Text>
-      <TextInput placeholder="Product to be traced"></TextInput>
-    </View>
-  );
+export default class App extends Component {
+  state = { product: '' , traceArray : [], isLoggedIn : true};
+
+    updateProduct = event =>{
+        this.setState({ product : event.target.value});
+    }
+
+    traceProduct = () => {
+        const { product } = this.state;
+
+        fetch('https://ancient-citadel-77688.herokuapp.com/api/trace', {
+            method: 'POST',
+            headers: { 'Content-Type' : 'application/json' },
+            body: JSON.stringify({product})
+    }).then(response => response.json())
+        .then(json => this.setState({ traceArray: json.traceArray})); 
+    }
+
+  render(){
+    const {traceArray,isLoggedIn} = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.titleText}>Trace a Product</Text>
+        <Text>Enter Product Name</Text>
+        <TextInput placeholder="Product to be traced" value={this.state.product} onChange={this.updateProduct}></TextInput>
+        <Button title="Trace" onPress={this.traceProduct}></Button>
+        { traceArray.length>0 && 
+                <View style={styles.container}>
+                  <Text>The Product is found at address: </Text>
+                  <FlatList>
+                  {traceArray.map(trace => <Text>{trace}</Text>)}
+                  </FlatList>
+        </View> }
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +46,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
